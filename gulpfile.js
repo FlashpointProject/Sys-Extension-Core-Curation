e5307abf5d96b0a4eed5c133575e9c6132854540
Colin <colinb969@gmail.com>
2024-04-03 12:35:01 +0100

    Swap to esbuild

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const gulp = require('gulp');
 const zip = require('gulp-zip');
 const merge = require('merge-stream');
+const esbuild = require('esbuild');
 
 const filesToCopy = [
     'extension.js',
@@ -11,6 +12,30 @@ const filesToCopy = [
     'LICENSE.md',
     'README.md'
 ];
+
+
+function build(done) {
+    esbuild.build({
+        bundle: true,
+        entryPoints: ['./src/extension.ts'],
+        outfile: './dist/extension.js',
+        platform: 'node',
+        external: ['flashpoint-launcher'],
+    })
+    .catch(console.error)
+    .finally(done);
+}
+
+async function watch() {
+    const ctx = await esbuild.context({
+        bundle: true,
+        entryPoints: ['./src/extension.ts'],
+        outfile: './dist/extension.js',
+        platform: 'node',
+        external: ['flashpoint-launcher'],
+    });
+    return ctx.watch();
+}
 
 function clean(cb) {
     fs.rm('./package', { recursive: true }, (err) => {
@@ -37,7 +62,6 @@ function package() {
     return gulp.src('package/**/*').pipe(zip('core-curation.fplx')).pipe(gulp.dest('.'));
 }
 
-exports.clean = clean;
-exports.stage = stage;
-exports.package = package;
-exports.default = series(clean, stage, package);
+exports.build = series(build);
+exports.watch = series(watch);
+exports.package = series(clean, stage, package);
